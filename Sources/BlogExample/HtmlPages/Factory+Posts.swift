bf18ab12e7d551fb1c0a3dc73d74bf2c3d6f02f9
Thomas Kellough <thomas.r.kellough@gmail.com>
2020-08-02 12:37:30 -0500

    added postContent node and a way to sort posts by date

--- Sources/BlogExample/HtmlPages/Factory+Posts.swift
+++ Sources/BlogExample/HtmlPages/Factory+Posts.swift
@@ -13,32 +13,7 @@ extension MyHTMLFactory {
          HTML(
             .head(for: context.index, on: context.site),
             .body(
-                // Create an unordered list
-                .ul(
-                    // Loop over each blog post that we have in our code
-                    .class("item-list"),
-                    .forEach(
-                        section.items
-                    ) { item in
-                        // Creates a list item for each post
-                        .li (
-                            // Creates an article node to display our information
-                            .article(
-                                // Creates a heading with our post title
-                                .h1(
-                                    // Creates an anchor tag so we can create the link to our post
-                                    .a(
-                                        // Creates the link to our post so we can click it and read everything
-                                        .href(item.path),
-                                        .text(item.title)
-                                    )
-                                ),
-                                // Creates a description  of what our post is about
-                                .p(.text(item.description))
-                            )
-                        )
-                    }
-                )
+                .postContent(for: section.items, on: context.site)
             )
         )
     }
